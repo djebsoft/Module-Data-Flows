8beb4f452213f4bb5ea3cb00422bddd4fc4ac9ed
djebsoft <djebbour1985@gmail.com>
2026-05-18 06:43:23 +0100

    updated the code to delete all rows in one operation

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -58,11 +58,7 @@ function Book(title, author, pages, check) {
 
 function render() {
   let table = document.getElementById("display");
-  let rowsNumber = table.rows.length;
-  //delete old table
-  for (let n = rowsNumber - 1; n > 0; n--) {
-    table.deleteRow(n);
-  }
+table.tBodies[0].innerHTML = "";
   //insert updated row and cells
   let length = myLibrary.length;
   for (let i = 0; i < length; i++) {
